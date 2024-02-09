b8f3c54f8fd3fb827fb7c1648648232a7f72314f
spham00 <84934173+spham00@users.noreply.github.com>
2024-02-09 17:15:02 -0500

    Create assignment8.js

---
+++ assignments/assignment8.js
@@ -0,0 +1,23 @@
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+
+// displaying for each book in the library
+library.forEach( (book) => {
+    console.log('Author: ' + book.author.toUpperCase() + '\nTitle: ' +
+        book.title.toUpperCase() + '\nReading Status: ' + 
+        book.readingStatus.toString().toUpperCase());
+});
