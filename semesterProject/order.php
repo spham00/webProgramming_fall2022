f745aad60e130714814234f9be808d2a222c5ad1
spham00 <84934173+spham00@users.noreply.github.com>
2024-02-09 20:36:12 -0500

    Create order.php

---
+++ semesterProject/order.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Order Now</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+
+    <!-- theme -->
+    <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/pepper-grinder/jquery-ui.css"/>
+   
+    <!-- include script source to CDN location of most recent jQuery vers -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- load jquery ui -->
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+</head>
+<body>
+    <!-- Header (contains page title) -->
+    <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/" class="header-link">
+        <div class="header">
+            <h1>Custom Eats</h1>
+        </div>
+    </a>
+
+    <!-- Navigation bar, used div to make it responsive -->
+    <nav>
+        <div class="navbar">
+            <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/about.html">About Us</a>
+            <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/filters.php">Customize Filters</a>
+            <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/find_restaurants.html">Find Restaurants</a>
+            <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/order.php">Order Now</a>
+            <a href="http://elvis.rowan.edu/~phamsa68/WebProgramming/SemesterProject/info.html">Food Info</a>
+        </div>
+    </nav>
+
+    <div class="colored-bg">
+        <h2>Order Now</h2>
+
+        <p>
+            <b>Directions:</b> Select the restaurant from the box below and hit
+            "Submit" to be redirected to the appropriate page for that restaurant.
+            From there, you can view the filtered menu and proceed with ordering.
+        </p>
+    </div>
+
+    
