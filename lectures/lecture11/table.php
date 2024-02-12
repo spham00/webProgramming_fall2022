bb768d1069ef3b77539ba8d0adfab01f49d4b383
spham00 <84934173+spham00@users.noreply.github.com>
2024-02-12 18:54:46 -0500

    Create table.php

---
+++ lectures/lecture11/table.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP page</title>
+    </head>
+    <body>
+        <!-- not sure why it's red fo rborder but it works -->
+        <table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+            <tr>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+            </tr>
+            <tr>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+                <td height=35px width=30px bgcolor=#FFFFFF>X</td>
+                <td height=35px width=30px bgcolor=#000000>X</td>
+            </tr>
+        </table>
+    </body>
+</html>
